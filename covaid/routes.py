8b2f397897a81f7de4ffc6e4f9d4bf8848285c78
shreygupta04 <shreygupta04@gmail.com>
2020-04-01 18:25:38 -0400

    home change

--- covaid/routes.py
+++ covaid/routes.py
@@ -5,7 +5,7 @@ from flask import render_template, url_for, flash, redirect
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('layout.html')
+    return render_template('home.html')
 
 
 @app.route("/about")
