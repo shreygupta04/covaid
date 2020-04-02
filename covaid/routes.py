0246883829ec2f726371b17a6ad857d10f353e3f
anirudhb23 <49347122+anirudhb23@users.noreply.github.com>
2020-04-02 17:30:31 -0400

    a

--- covaid/routes.py
+++ covaid/routes.py
@@ -1,4 +1,5 @@
 from covaid import app
+from covaid.forms import RegistrationForm, LoginForm
 from flask import render_template, url_for, flash, redirect
 
 
@@ -15,15 +16,29 @@ def about():
 
 @app.route("/contact")
 def contact():
-    return "<h1>Home<h1>"
+    return render_template('contact.html')
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return "<h1>Home<h1>"
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
-
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+<<<<<<< HEAD
     return "<h1>Home<h1>"
+=======
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@covaid.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
+>>>>>>> 09befd9f24f88bafebbb39e4b4c7332c9cf7c1f4
