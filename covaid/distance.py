d62b59ce4bb6c8290cd3811a35067f6c7f31531a
shreygupta04 <shreygupta04@gmail.com>
2020-04-08 18:07:23 -0400

    Update distance.py

--- covaid/distance.py
+++ covaid/distance.py
@@ -1,7 +1,9 @@
 import requests
+import json
+import secrets
 
-API_KEY = 'AIzaSyADuVGqvUTqN-LSk8zen6PufXuJP9wHN2Y'
 
-response = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Oakton,3413,Waples,Glen,Court&destinations=Ashburn,23021,Olympia,Drive&key=' + API_KEY)
+response = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Oakton,3413,Waples,Glen,Court&destinations=Ashburn,23021,Olympia,Drive&key=' + secrets.API_KEY)
 
 data = response.json()
+print(data)
